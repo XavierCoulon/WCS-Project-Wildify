a5f901f6e1511f18732e6b86a28cb782085ce802
XavierCoulon <xcoulon.mail@gmail.com>
2022-11-10 19:35:11 +0100

    change on playlist system

--- frontend/src/components/PlaylistsList/index.jsx
+++ frontend/src/components/PlaylistsList/index.jsx
@@ -1,28 +1,55 @@
+import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import TrackList from "../TrackList";
 import { playlistsFetcher } from "../../utils/axiosTools";
 
-function PlaylistsList() {
+function PlaylistsList({ handleCurrentId }) {
   const [list, setList] = useState(null);
+  const [currentList, setCurrentList] = useState(null);
+  const [tracks, setTracks] = useState(null);
+
+  const load = () =>
+    playlistsFetcher.getAll().then((result) => {
+      setList(result);
+      setCurrentList(result[0].id);
+    });
 
   useEffect(() => {
-    playlistsFetcher.getAll().then((result) => setList(result));
+    load();
   }, []);
 
-  if (!list) return <div>Loading ...</div>;
+  useEffect(() => {
+    if (currentList) {
+      playlistsFetcher
+        .getTracks(currentList)
+        .then((result) => setTracks(result.songs));
+    }
+  }, [currentList]);
+
+  if (!tracks) return <div>Loading ...</div>;
 
   return (
-    <div className="grid grid-cols-4 p-5">
-      {list.map((playlist) => (
-        <Link
-          className=" bg-gray opacity-90 rounded-md my-1 text-white items-center text-center p-1 m-1 "
-          to={`/playlists/${playlist.id}`}
-        >
-          {playlist.title}
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-4 p-5">
+        {list.map((playlist) => (
+          <span
+            className="cursor-pointer bg-gray opacity-90 rounded-md my-1 text-white items-center text-center p-1 m-1 "
+            aria-hidden="true"
+            onClick={() => {
+              setCurrentList(playlist.id);
+            }}
+          >
+            {playlist.title}
+          </span>
+        ))}
+      </div>
+      <TrackList tracks={tracks} handleCurrentId={handleCurrentId} />
+    </>
   );
 }
 
 export default PlaylistsList;
+
+PlaylistsList.propTypes = {
+  handleCurrentId: PropTypes.func.isRequired,
+};
