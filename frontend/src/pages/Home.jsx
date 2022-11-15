dfa65e488dcfa2b62fcfad7f9b1e33c4698a355f
Pupuce-64 <laeti.decandt@gmail.com>
2022-11-15 17:18:23 +0100

    worked on background

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import TrackList from "../components/TrackList";
 import RecentlyPlayed from "../components/RecentlyPlayed";
 import GenresList from "../components/GenresList/index_original";
 import { songsFetcher } from "../utils/axiosTools";
+import WaveImg from "../utils/waveImg";
+import wave from "../assets/wave.png";
 
 function Home({ handleCurrentId }) {
   const [tracks, setTracks] = useState([]);
@@ -18,7 +20,16 @@ function Home({ handleCurrentId }) {
   if (!tracks) return <div>Loading ...</div>;
 
   return (
-    <div className="bg-[#F3E8F3] dark:bg-slate-800 w-full text-black dark:text-white">
+    <div className="bg-white dark:bg-slate-800 w-full text-black dark:text-white">
+      <div className="flex justify-center">
+        <img
+          className="w-5/6 h-full mx-14 mt-5 absolute opacity-25"
+          src={wave}
+          alt="wave de fond musicale"
+        />
+        <WaveImg />
+      </div>
+
       <GenresList handleCurrentId={handleCurrentId} />
       <RecentlyPlayed handleCurrentId={handleCurrentId} />
       <TrackList tracks={tracks} handleCurrentId={handleCurrentId} />
