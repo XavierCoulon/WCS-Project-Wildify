0d525a7223522ba1209834287b83649f23012427
Tony Creavin <tonycreavin@gmail.com>
2022-11-17 13:55:33 +0100

    track player fixed, logo changed, searchbar text-color changed, scroll problem fixed playlist background fixed

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({ setIsMenu, isMenu }) {
   const { handleToggleDarkMode } = useContext(ThemeContext);
 
   return (
-    <div className="bg-[#F3E8F3] fixed top-0 z-50 dark:bg-grayCustom p-6 flex justify-between items-center align-middle w-full h-[96px]">
+    <div className="bg-[#F3E8F3] fixed top-0 z-50 dark:bg-slate-800 p-6 flex justify-between items-center align-middle w-full h-[96px]">
       {isMenu ? (
         <img
           onClick={() => setIsMenu((state) => !state)}
@@ -26,18 +26,22 @@ function Navbar({ setIsMenu, isMenu }) {
       )}
       <input
         type="text"
-        className="dark:bg-slate-800 bg-white rounded-lg w-3/5 px-3 py-1 border-0 text-slate-400 focus:border-0 lg:ml-28"
+        className="dark:bg-slate-800 dark:placeholder-white placeholder-black bg-white rounded-lg w-3/5 px-3 py-1 border-2  text-black dark:text-white focus:border-0 lg:ml-28"
         placeholder="Search..."
       />
       <button
         onClick={handleToggleDarkMode}
         type="button"
-        className="h-8 w-15 rounded-lg px-1 text-white dark:text-black bg-[#F3E8F3] dark:bg-[#F3E8F3] "
+        className="h-8 w-15 rounded-lg px-1  bg-[#F3E8F3] dark:bg-[#F3E8F3] "
       >
         🌓
       </button>
 
-      <img src="src/assets/logo.png" alt="logo" className="w-10" />
+      <img
+        src="src/assets/wildifyLogo.png"
+        alt="logo"
+        className=" rounded-md w-12"
+      />
     </div>
   );
 }
