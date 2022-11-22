09509d7bb8c8d0d6d89f91d4192477aef7ad109b
Pupuce-64 <laeti.decandt@gmail.com>
2022-11-22 15:51:07 +0100

    added the fouvourites count on favourites page by lifting up the favourite state

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
-import FavouritesList from "../components/FavouritesList";
+import { useState } from "react";
 import favImg from "../assets/Lead-image.png";
 import favBg from "../assets/Favorite_bg.png";
+import storage from "../utils/localStorageTools";
+import FavouritesList from "../components/FavouritesList";
 
 function Favourites({ handleCurrentId }) {
+  const [favourites, setFavourites] = useState(() => storage.get("favorite"));
   return (
     <div className="bg-pinkCustom dark:bg-blackCustom w-full h-screen text-black dark:text-white">
       <div className="flex relative lg:h-3/6 h-1/5 overflow-hidden">
@@ -19,12 +22,32 @@ function Favourites({ handleCurrentId }) {
           src={favImg}
           alt="favorite theme"
         />
-        <h1 className="lg:text-5xl font-bold mt-10 lg:ml-20 ml-4 font-mono">
-          Favourites
-        </h1>
+
+        <div className="flex flex-col justify-between">
+          <div>
+            <h1 className="lg:text-5xl font-bold mt-10 lg:ml-20 ml-4 font-mono">
+              Favourites
+            </h1>
+          </div>
+
+          <div className="flex justify-center ">
+            <p className="pb-14 flex justify-center items-center font-medium">
+              <img
+                className="w-10 h-10 mr-5"
+                src="https://upload.wikimedia.org/wikipedia/commons/3/35/Emoji_u2665.svg"
+                alt="coeur"
+              />
+              {favourites.length} favourites songs
+            </p>
+          </div>
+        </div>
       </div>
       <div className="mt-5 pb-24">
-        <FavouritesList handleCurrentId={handleCurrentId} />
+        <FavouritesList
+          favourites={favourites}
+          setFavorites={setFavourites}
+          handleCurrentId={handleCurrentId}
+        />
       </div>
     </div>
   );
